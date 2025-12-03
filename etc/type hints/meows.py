94b1c3c77db92241280229db0891e70c5b5b4196
mohit98thaker <mohuace@gmail.com>
2025-12-02 22:29:30 -0500

    type hints

--- etc/type hints/meows.py
+++ etc/type hints/meows.py
@@ -1,9 +1,18 @@
 # This class is for type hints in python
 
-def meow(n: int):
-    for _ in range(n):
-        print("Meow!")
+def meow(n: int) -> str:
+    """ 
+    This is the convention for docstring.
+    This is how you document a function in Python.
+    Return a string with n meows, each on its own line.
+    :type n: int
+    :raise TypeError: if n is not an int
+    :return: string with n meows
+
+    """
+    return "meow\n" * n
 
 
 number = int(input("Enter number of meows: "))
-meow(number)
+meows: str = meow(number)
+print(meows, end="")
